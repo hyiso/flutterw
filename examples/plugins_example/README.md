d61259c3008dcf2cd5843aa7565e8c7e66e04858
hyiso <mylaoda@gmail.com>
2022-11-09 22:56:30 +0800

    Remove test and set publish_to:none in each example

---
+++ examples/plugins_example/README.md
@@ -0,0 +1,9 @@
+## Getting started
+
+Clone this repo
+
+cd examples/plugins_example
+
+flutterw clean
+
+flutterw pub get
---
+++ examples/plugins_example/lib/plugins_example.dart
@@ -0,0 +1,7 @@
+library plugins_example;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
