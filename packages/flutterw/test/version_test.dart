bf601bf7412126a6fad45651cf7028adcd27efbc
hyiso <mylaoda@gmail.com>
2023-01-04 23:02:27 +0800

    Add version test to flutterw tests

---
+++ packages/flutterw/test/version_test.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+import 'package:flutterw/version.g.dart';
+import 'package:test/test.dart';
+import 'package:yaml/yaml.dart';
+
+void main() {
+  test('lib/version.g.dart should be up-to-date.', () {
+    final yamlMap = loadYaml(File('pubspec.yaml').readAsStringSync()) as YamlMap;
+    final yamlVersion = yamlMap['version'] as String;
+    expect(kPackageVersion, equals(yamlVersion));
+  });
+}
