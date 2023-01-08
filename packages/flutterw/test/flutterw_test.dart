9b544c7649e2232acc5cc4ad6ade2d845ceacec1
hyiso <mylaoda@gmail.com>
2023-01-08 21:58:29 +0800

    fix: flutterw_test

--- packages/flutterw/test/flutterw_test.dart
+++ packages/flutterw/test/flutterw_test.dart
@@ -1,22 +1,19 @@
 import 'dart:io';
 
 import 'package:flutterw/flutterw.dart';
-import 'package:path/path.dart' show join;
 import 'package:test/test.dart';
 
 void main() {
-  group('flutterw should work', (() {
-    final outputFile = File(join('lib', 'src', 'version.g.dart'));
-    final outputContent = outputFile.readAsStringSync();
-    tearDownAll(() => outputFile.writeAsStringSync(outputContent));
-    test('with hooks.', () async {
-      final runner = FlutterwRunner(
-          config: FlutterwConfig.fromFile(File('flutterw.yaml')));
-      outputFile.writeAsStringSync('');
-      expect(outputFile.readAsStringSync(), equals(''));
+  test('flutterw should work.', () async {
+    final runner = FlutterwRunner(
+        config: FlutterwConfig.empty());
+    final dartToolDir = Directory('.dart_tool');
+    expect(dartToolDir.existsSync(), true);
 
-      await runner.run(['pub', 'publish', '--dry-run']);
-      expect(outputFile.readAsStringSync(), outputContent);
-    });
-  }));
+    await runner.run(['clean']);
+    expect(dartToolDir.existsSync(), false);
+
+    await runner.run(['pub', 'get']);
+    expect(dartToolDir.existsSync(), true);
+  });
 }
