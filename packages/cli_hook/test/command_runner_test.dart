612a2c0dbf86e080760a6b01cde940d6bc5fd6d7
hyiso <mylaoda@gmail.com>
2023-01-28 22:10:23 +0800

    chore(cli_hook): add more test

--- packages/cli_hook/test/command_runner_test.dart
+++ packages/cli_hook/test/command_runner_test.dart
@@ -1,4 +1,3 @@
-import 'package:args/command_runner.dart';
 import 'package:test/test.dart';
 
 import 'common.dart';
@@ -8,7 +7,7 @@ void main() {
     final preFooHook = TestHook('pre:foo');
     final postBarHook = TestHook('post:bar');
     test('for command', () async {
-      final runner = CommandRunner('test', 'test');
+      final runner = TestRunner({});
       runner.addCommand(TestCommand('foo'));
       runner.addCommand(TestCommand('bar'));
       await runner.run(['foo']);
@@ -19,8 +18,8 @@ void main() {
       expect(postBarHook.isTriggered, false);
     });
 
-    test('for command subcommand', () async {
-      final runner = CommandRunner('test', 'test');
+    test('for subcommand', () async {
+      final runner = TestRunner({});
       runner.addCommand(TestCommand('foo')..addSubcommand(TestCommand('bar')));
       await runner.run(['foo', 'bar']);
       expect(preFooHook.isTriggered, false);
@@ -28,7 +27,7 @@ void main() {
     });
   }));
 
-  group('HookRunner with hooks should run', (() {
+  group('HookRunner with pre and post hooks should run', (() {
     test('for command', () async {
       final preFooHook = TestHook('pre:foo');
       final postBarHook = TestHook('post:bar');
@@ -37,10 +36,8 @@ void main() {
         postBarHook.name: postBarHook,
       };
       final runner = TestRunner(hooks);
-      final fooCommand = TestCommand('foo');
-      final barCommand = TestCommand('bar');
-      runner.addCommand(fooCommand);
-      runner.addCommand(barCommand);
+      runner.addCommand(TestCommand('foo'));
+      runner.addCommand(TestCommand('bar'));
       await runner.run(['foo']);
       expect(preFooHook.isTriggered, true);
       expect(postBarHook.isTriggered, false);
@@ -48,7 +45,7 @@ void main() {
       expect(postBarHook.isTriggered, true);
     });
 
-    test('for command subcommand', () async {
+    test('for subcommand', () async {
       final preFooHook = TestHook('pre:foo');
       final preFooBarHook = TestHook('pre:foo:bar');
       final postFooHook = TestHook('post:foo');
@@ -58,14 +55,49 @@ void main() {
         postFooHook.name: postFooHook,
       };
       final runner = TestRunner(hooks);
-      final fooCommand = TestCommand('foo');
-      final barCommand = TestCommand('bar');
-      runner.addCommand(fooCommand);
-      fooCommand.addSubcommand(barCommand);
+      runner.addCommand(TestCommand('foo')..addSubcommand(TestCommand('bar')));
       await runner.run(['foo', 'bar']);
       expect(preFooHook.isTriggered, false);
       expect(preFooBarHook.isTriggered, true);
       expect(postFooHook.isTriggered, true);
+    });
+  }));
+
+  group('HookRunner with command hooks should run', (() {
+    test('for command', () async {
+      final fooHook = TestHook('foo');
+      final hooks = {
+        fooHook.name: fooHook,
+      };
+      final runner = TestRunner(hooks);
+      runner.addCommand(TestCommand('foo'));
+      await runner.run(['foo']);
+      expect(fooHook.isTriggered, true);
+    });
+
+    test('for subcommand', () async {
+      final fooHook = TestHook('foo');
+      final fooBarHook = TestHook('foo:bar');
+      final hooks = {
+        fooHook.name: fooHook,
+        fooBarHook.name: fooBarHook,
+      };
+      final runner = TestRunner(hooks);
+      await runner.run(['foo', 'bar']);
+      expect(fooHook.isTriggered, false);
+      expect(fooBarHook.isTriggered, true);
+    });
+
+    test('for colon-joined command', () async {
+      final fooHook = TestHook('foo');
+      final fooBarHook = TestHook('foo:bar');
+      final hooks = {
+        fooHook.name: fooHook,
+        fooBarHook.name: fooBarHook,
+      };
+      await TestRunner(hooks).run(['foo:bar']);
+      expect(fooHook.isTriggered, false);
+      expect(fooBarHook.isTriggered, true);
     });
   }));
 }
